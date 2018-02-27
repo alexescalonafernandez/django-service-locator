fa050ca592d4c14a0ef4a09e12be1f6d8b36a565
Alexander Escalona Fernandez <alexescalonafernandez@gmail.com>
2018-02-27 11:29:24 -0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-service-locator',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/alexescalonafernandez/django-service-locator.git',
+    license='MIT',
+    author='Alexander Escalona Fernández',
+    author_email='alexescalonafernandez@gmail.com',
+    setup_requires=['setuptools-markdown', 'service-locator'],
+    description='Django application that allows the use of a light implementation of the service locator pattern.',
+    long_description_markdown_filename='README.md',
+    classifiers=[
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Pattern Design :: Service Locator',
+        ],
+)
